c38d456dc326a5bcfe89693101aada5c3bf1a57d
Thomas Warford <thomasdwarford@hotmail.co.uk>
2022-07-12 14:57:58 +0100

    Changed dimensions of Ensemble.q and Ensemble.p to allow easier broadcasting.

--- src/ensemble.py
+++ src/ensemble.py
@@ -6,8 +6,9 @@ class Ensemble( ):
     def __init__(self, numParticles, numDimensions):
         
         self.numParticles = numParticles
-        self.q = np.zeros((numParticles, numDimensions))
-        self.p = np.zeros((numParticles, numDimensions))
+        self.numDimensions = numDimensions
+        self.q = np.zeros((numDimensions, numParticles))
+        self.p = np.zeros((numDimensions, numParticles))
         self.m = np.zeros(numParticles)
         self.weights = np.zeros(numParticles)
 
@@ -19,5 +20,34 @@ class Ensemble( ):
             raise IndexError(f'Index {particleNum} out of bounds. '\
                 f'numParticles={self.numParticles}') 
 
-        return self.q[particleNum], self.p[particleNum], self.m[particleNum], \
-            self.weights[particleNum]
+        return self.q[:, particleNum], self.p[:, particleNum], \
+        self.m[particleNum], self.weights[particleNum]
+
+## MOVE TO TEST - I DONT GET VC
+def main( ):
+    numDimensions = 4
+    numParticles = 100
+
+    ensemble1 = Ensemble(numParticles, numDimensions)
+
+    # expected output
+    qExp = np.zeros( numDimensions )
+    pExp = np.zeros( numDimensions )
+    mExp = 0
+    wExp = 0
+
+    q1, p1, m1, w1 = ensemble1.particle( 10 )
+
+    if (qExp == q1).all( ) and (pExp == p1).all( ) and (mExp == m1).all( ) and \
+        (wExp == w1).all( ):
+        print('Test 1 passed.')
+
+    try:
+        _ = ensemble1.particle(numParticles + 1)
+        print('Test 2 Failed')
+    except IndexError as error: 
+        print(error)
+        print('Test 2 Passed')
+
+if __name__ == '__main__':
+    main()
