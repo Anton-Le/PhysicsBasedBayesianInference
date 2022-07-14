84273a1becafd80ce31b78ae1c0b62645dbd4dc1
ThomasWarford <64084106+ThomasWarford@users.noreply.github.com>
2022-07-14 16:03:00 +0100

    Feature ensemble and integrator (#1)
    Added ensemble class and unfinished integrator class. Removed tests.

---
+++ src/ensemble.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3 
+
+import numpy as np
+from scipy.stats import norm
+from scipy.constants import k as boltzmannConst
+
+class Ensemble( ):
+    ''' Class containing particle positions, momenta and masses as well as weights and 
+    a potential taking only position as an argument.
+    '''
+    potenial = None # If None integrator runs N-body simulation, 
+                    # use potential.noPotential for free particles
+
+    def __init__(self, numDimensions, numParticles, potential):
+        
+        self.numParticles = numParticles
+        self.numDimensions = numDimensions
+        self.q = np.zeros((numDimensions, numParticles))
+        self.p = np.zeros((numDimensions, numParticles))
+        self.mass = np.zeros(numParticles)
+        self.weights = np.zeros(numParticles)
+        self.potential = potential
+
+
+    def setWeights(self, temperature):
+        hamiltonian = self.potential(self.q) + self.p ** 2 / (2 * self.mass)
+        self.weights = np.exp(- hamiltonian / boltzmannConst * temperature)
+
+    def initializeThermal(self, mass, temperature, q_std):
+        ''' Distribute momentum based on a thermal distribution and position
+        with a normal distribution.
+
+        '''
+        if len(mass) != self.numParticles or np.ndim(mass) != 1:
+            raise ValueError('Mass must be 1D array of length numParticles.')
+
+        self.mass = mass
+        
+        self.q = norm.rvs(scale=q_std,
+            size=(self.numDimensions, self.numParticles))
+        
+        # thermal distribution
+        p_std = np.sqrt(self.mass * boltzmannConst * temperature)
+        self.p = norm.rvs(scale=p_std, size=(self.numDimensions,
+            self.numParticles))
+
+        # set weights
+        self.setWeights(temperature)
+        
+
+    def __iter__(self):
+        ''' Allows for easy unpacking of ensemble object.
+        '''
+        return self.q, self.p, self.mass, self.weights, self.potential
+
+
+    def particle(self, particleNum):
+        ''' Return information about the particleNum th particle.
+        '''      
+        if not 0 <= particleNum < self.numParticles:
+
+            raise IndexError(f'Index {particleNum} out of bounds. '\
+                f'numParticles={self.numParticles}') 
+
+        return self.q[:, particleNum], self.p[:, particleNum], \
+        self.mass[particleNum], self.weights[particleNum]
---
+++ src/potential.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3 
+
+import numpy as np
+
+def harmonicPotentialND(q, k):
+	if q.shape[0] != len(k):
+		raise ValueError('k must be 1D array corresponding with spring constant \
+			in 3 dimensions.')
+	return 0.5 * k * q ** 2
+
+def noPotential(q):
+	return 0
