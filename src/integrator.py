890d8532ccac08d1a25496ebcedade4101cd063e
bruno rodriguez <bruno.rodriguezcarrillo@epfl.ch>
2022-07-15 15:39:15 +0200

    Implementation of integrators Leapfrog and Stormer-Verlet. I assume that the potential function V is given and then, either method needs a potential function to work with. That is, we do not use the force F or accelaration to numerical solve positons and momenta but the potential function using the fact that force = -dV/dx, where x represents position

--- src/integrator.py
+++ src/integrator.py
@@ -1,50 +1,123 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Jul 14 2022
+@author: bruno
+
+file containing functions to compute the numerical solution of 
+momenta and positions for a N-particle system
+
+"""
 
 import numpy as np
-
-class Integrator():
-    def integrationMethod():
-        raise NotImplementedError('Integrator superclass does not specify \
-            integration method')
-    
-    def plottingIntegrationMethod():
-        raise NotImplementedError('Method not defined.')
-
-
-    def __init__(self, ensemble, dt, numSteps, ds):
-        self.q, self.p, self.mass, self.weights, self.potential = ensemble
-        self.dq = dq
-
-
-    def getAccel(self):
-        plus = self.potential(self.q + self.dq)
-        minus = self.potential(self.q - self.dq)
-        force = (minus - plus) / (2 * self.dq)
-        return force / self.mass
-
-
-class Leapfrog(Integrator):
-
-
-    def integrationMethod(self):
-        v = self.p / self.mass 
-        aCurrent = self.getAccel()
+from ensemble import Ensemble
+   
+class integrator_methods:       
+    """
+    @description:
+        This class contains functions to compute the positions 
+        and momenta at final simulation time finalTime for a system of N particles 
+        interacting according to a given potential.
+        We do not store the solution at each time step, but the solution at final simulation
+        time. 
+        We assume that the particles are described by momentum and position vectors in D dimensions.
         
-        for i in range(numSteps):
-            self.ensemble.x += v * dt + 0.5 * a_current * dt ** 2
+    @parameters:        
+        samples (numpy array): train/test data points
+        labels (numpy array): contains the samples/data labels
+    """
+    
+    def __init__(self, ensemble, stepSize, finalTime):
+        # initial positions 
+        self.q = np.copy(ensemble.q)
+        # initial momenta 
+        self.p = np.copy(ensemble.p)
+        self.numParticles = ensemble.numParticles
+        # step size for integrator
+        self.stepSize = stepSize
+        # final simulation time
+        self.finalTime = finalTime        
+        self.totalSteps = int(self.finalTime/self.stepSize)
+        # integrator to use
+        self.method = None        
+        self.potentialFunction = ensemble.potential
+        # value of potential for a given configuration q
+        self.potential = np.zeros_like(self.q)
+    
+    def leap_frog(self):
+        """
+        @description:
+            function to compute numerically positions and momenta for N particles 
+            using leap frog algorithm
+            Algorithm taken from https://www2.math.ethz.ch/education/bachelor/seminars/fs2008/nas/crivelli.pdf
             
-            aNext = self.getAccel
-
-            v += 0.5 * (aNext + aCurrent)
-
-
-            aCurrent = aNext
-
-        self.p = v * self.mass 
-
+        @parameters:        
+            ensemble (class): initilialize particles and contains information about particles
+                              positons and momente
+            stepSize (float): step size for numerical integrator
+            finalTime (float): final simulation time
+        """
+        # first value of potential
+        for i in range(self.numParticles):            
+            self.potential[:, i] = self.potentialFunction(self.q[:, i]) 
+            
+        # number of time steps consider on [initialTime, finalTime]
+        for i in range(self.totalSteps):             
+            for j in range(self.numParticles):   
+                # half step in leap frog
+                pMidStep = self.p[:, j] - 0.50 * self.stepSize * self.potential[:, j]
+                self.q[:, j]  = self.q[:, j] + self.stepSize * pMidStep
+                self.p[:, j]  = pMidStep - 0.50 * self.stepSize * self.potential[:, j]  
+                # update potential
+                self.potential[:, j]  = self.potentialFunction(self.q[:, j])
+            
+        # return postion and momenta of all particles at finalTime
         return self.q, self.p
-
-    def plottingIntegrationMethod(self):
-        return 0
+    
+    def stormer_verlet(self):
+        """
+        @description:
+            function to compute numerically positions and momenta for N particles 
+            using Stormer-Verlet frog algorithm. 
+            Algorithm taken from https://www2.math.ethz.ch/education/bachelor/seminars/fs2008/nas/crivelli.pdf
+            
+        @parameters:        
+            ensemble (class): initilialize particles and contains information about particles
+                              positons and momente
+            stepSize (float): step size for numerical integrator
+            finalTime (float): final simulation time
+        """
+        # first value of potential
+        for i in range(self.numParticles):            
+            self.potential[:, i] = self.potentialFunction(self.q[:, i]) 
+        
+        # number of time steps consider on [initialTime, finalTime]
+        for i in range(self.totalSteps):
+            for j in range(self.numParticles):
+                self.q[:, j] = self.q[:, j] + self.stepSize * ( self.p[:, j] -  0.50 * self.stepSize * self.potential[:, j] )            
+                newPotential = self.potentialFunction(self.q[:, j])
+                self.p[:, j] = self.q[:, j] - 0.50 * self.stepSize * ( self.potential[:, j] + newPotential)
+                self.potential[:, j] = newPotential         
+        
+        # return postion and momenta of all particles at finalTime
+        return self.q, self.p
+    
+    def numerical_solution(self, method = 'LP'):
+        self.method = method
+        if self.method == 'LP':
+            # solve trajectories for each particle
+            self.leap_frog()
+            # return matrix containing numerical solutions for all particles
+            return self.q, self.p
+        
+        if self.method == 'SV':
+            self.stormer_verlet()
+            return self.q, self.p
+        
+        else:
+            raise Exception('Choose a valid numerical integrator')
+                        
+def toy_potential(x):
+    return np.sin(x) + x
 
 
--- src/test_integrator.py
+++ src/test_integrator.py
@@ -1,13 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 15 2022
+
+@author: bruno
+"""
+
 from ensemble import Ensemble
-from integrator import Leapfrog
+from integrator import integrator_methods
 import numpy as np
 
 def harmonicPotential(q):
-	return q
+    return q
 
-ensemble = Ensemble(100, 4)
-
-leapfrog = Leapfrog(ensemble, 0.01, 1000, harmonicPotential, 0.01)
-
-leapfrog.getAccel()
-
+########################
+def main():
+    # integrator setup
+    finalTime = 1
+    stepSize = 0.01
+    method = 'SV'
+    # ensemble setup
+    numDimensions = 100
+    numParticles = 100
+    mass = np.ones(numParticles)
+    temperature = 1
+    q_std = 0.1
+    
+    #ensemble initialization
+    ensemble1 = Ensemble(numDimensions, numParticles, harmonicPotential)
+    ensemble1.initializeThermal(mass, temperature, q_std)
+    
+    # object of class integartor
+    sol_p_q = integrator_methods(ensemble1, stepSize, finalTime)
+    
+    # actual solution for position and momenta
+    p, q = sol_p_q.numerical_solution(method)
+    return p, q
+        
+if __name__ == '__main__':
+    p, q = main()
