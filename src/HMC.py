c81ad0d7750517bd87a02c15a52a1346aff988ce
bruno rodriguez <bruno.rodriguezcarrillo@epfl.ch>
2022-07-26 21:19:38 +0200

    Added[2~ potential.py file. I modified the HMC implementation by adding a few lines of code where it is checked whether we are given the gradient of the potential or not

--- src/HMC.py
+++ src/HMC.py
@@ -13,26 +13,40 @@ from jax import grad
 from integrator import leap_frog, stormer_verlet
 
 class HMC:
-    def __init__(self, ensemble, samples, density, integrator, stepSize, finalTime):
+    def __init__(self, ensemble, samples, density, integrator, stepSize, finalTime, potential=None, gradient = None):
         # we gather all information from the ensemble
         self.ensemble = ensemble
         self.samples = samples
         
-        self.density = density
+        # setting potential interaction
+        # if we are given the potential
+        if potential:
+            self.potential = potential
+        # if we are given the density we want to sample from 
+        else:
+            self.density = density
+            self.potential = self.potential_energy            
+        # if we are given the gradient of the potential, i.e., the force field
+        if gradient:
+            self.gradient = gradient
+        else:
+            self.gradient = None        
+
         self.stepSize= stepSize
         self.finalTime = finalTime
         self.integrator = integrator
     
     # negative log potential energy, depends on position q only
     # U(x) = -log( p(x) ) 
-    def potential_energy(self, x):
+    def potential_energy(self, x):    
+        # if we are given the density we want to sample from
         return -jnp.log( self.density(x) )            
     
     def exponential_hamiltonian(self, q, p):   
         energy = np.zeros(self.ensemble.numParticles)
         for i in range(self.ensemble.numParticles):
             # H = kinetic_energy + potential_energy
-            H = 0.5*jnp.linalg.norm(self.ensemble.p[:, i])**2 + self.potential_energy(self.ensemble.q[:, i])
+            H = 0.5*jnp.linalg.norm(self.ensemble.p[:, i])**2 + self.potential(self.ensemble.q[:, i])
             energy[i] = jnp.exp(-H)                    
         return energy
     
@@ -44,11 +58,12 @@ class HMC:
         
     def hmc_sample(self):
         
-        # mean and variance for momenta, these are fixed
+        # mean and variance for momenta, these are fixed. Standard multidimensional Gaussian 
         mean = np.zeros(self.ensemble.numDimensions)
         cov = np.identity(self.ensemble.numDimensions)        
         
-        # to store samples generated during HMC iteration
+        # to store samples generated during HMC iteration. 
+        # This is an array of matrices, each matrix corresponds to an HMC sample
         samples_hmc = np.zeros(self.ensemble.numParticles*self.ensemble.numDimensions*self.samples).reshape((self.ensemble.numDimensions,self.ensemble.numParticles, -1))
         
         self.print_information()
@@ -63,12 +78,22 @@ class HMC:
             # solve numerically for positions and momenta
             # notice that we pass the gradient of the potential function 
             if 'lp' == self.integrator:
-                num_sol = leap_frog(self.ensemble, grad(self.potential_energy), self.stepSize, self.finalTime)       
+                if self.gradient:
+                    num_sol = leap_frog(self.ensemble, self.gradient, self.stepSize, self.finalTime) 
+                else:
+                    # we approximate the gradient of the potential
+                    num_sol = leap_frog(self.ensemble, grad(self.potential), self.stepSize, self.finalTime)       
                                 
-            if 'sv' == self.integrator :
-                num_sol = stormer_verlet(self.ensemble, grad(self.potential_energy), self.stepSize, self.finalTime)
-                        
+            if 'sv' == self.integrator:
+                if self.gradient:                                           
+                    num_sol = stormer_verlet(self.ensemble, self.gradient, self.stepSize, self.finalTime)
+                else:
+                    # we approximate the gradient of the potential
+                    num_sol = stormer_verlet(self.ensemble, grad(self.potential), self.stepSize, self.finalTime)
+            
+            # numerical solution for momenta and positions
             newPosition, newMomentum = num_sol.integrate()
+            
             # flip momenta
             newMomentum = -newMomentum
 
@@ -78,17 +103,20 @@ class HMC:
             acceptanceProb = np.minimum(1, ratio)
             
             u = (np.random.rand(self.ensemble.numParticles))
+            
             mask = u < acceptanceProb
             mask = np.tile(mask, (self.ensemble.numDimensions, 1))            
             self.ensemble.q[mask] = newPosition[mask]
+            # update accepted moves
             samples_hmc[:, :, i] = self.ensemble.q
             #print('------------')
             
         return samples_hmc
-            
+    
+# toy examples to test HMC implementation on a 2D std Gaussian distribution
 def density(x):
     return jnp.exp( -0.50*jnp.linalg.norm(x)**2 ) / jnp.sqrt((2*jnp.pi)**2) 
-
+# this is passed to the integrator
 def potential(x):    
     return -jnp.log( density(x) )
                 
---
+++ src/potential.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Jul 14 2022
+@author: thomas
+
+File contain potentials & function to calculate n-body gravitational force.
+
+"""
+
+import numpy as np
+from scipy.constants import G as gravConst
+from numbers import Real # check if variable is number
+from scipy.optimize import approx_fprime
+
+def harmonicPotentialND(q, springConsts):
+    """
+    @description:
+        Returns harmonic potential at points given by q.
+        
+    @parameters:        
+        q (ndarray): numDimensions x numParticles array of positions
+        springConsts (ndarray): numDimensions array of spring constants
+    """
+
+    return 0.5 * np.dot(springConsts, q ** 2)
+
+
+def gravitationalPotential(r1, r2, mass1, mass2):
+    """
+    @description:
+        Returns potential between two masses.
+        
+    @parameters:        
+        r1 (ndarray): Position vector of first mass.
+        r2 (ndarray): Position vector of second mass.
+        mass1 (ndarray): First mass
+        mass2 (ndarray): Second mass
+    """
+    r = r1 - r2
+    distance = np.sqrt(np.dot(r, r))
+    return gravConst * mass1 * mass2 / distance
+
+
+def nBodyPotential(q, mass, shape=None):
+    """
+    @description:
+        Calculate n body potential for gravitational force.
+        
+    @parameters:        
+        q (ndarray): numDimensions x numParticles array of positions
+        mass (ndarray): numParticles array of masses
+        shape (array-like): original shape of q in case q is 1D array.
+    """
+
+    if shape != None: # approx_fprime requires q to be 1D array - Fixed here:
+        q = q.reshape(shape)
+
+    remainingParticles = q.shape[1]
+    potential = 0
+    countedParticles = 0
+    for particleNum_i in range(q.shape[1]):
+        remainingParticles -= 1
+        countedParticles += 1
+
+        for particleNum_j in range(remainingParticles):
+
+            potential += gravitationalPotential(
+                q[:, particleNum_i], 
+                q[:, countedParticles + particleNum_j],
+                mass[particleNum_i],
+                mass[countedParticles + particleNum_j]
+                )
+
+    return potential
+
+def nBodyForce(q, mass):
+    """
+    @description:
+        Calculate n body potential for gravitational force.
+        
+    @parameters:        
+        q (ndarray): numDimensions x numParticles array of positions
+        mass (ndarray): numParticles array of masses
+    """
+
+    outputShape = q.shape
+    gradient = approx_fprime(np.ravel(q), nBodyPotential, 1.49e-08, 
+        mass, outputShape)
+    gradient = gradient.reshape(outputShape)
+    return gradient
+
+
+def getForce(q, potentialFunc, dq):
+    """
+    @description:
+        Calculate forces at each position vector.
+        
+    @parameters:        
+        q (ndarray): numDimensions x numParticles array of positions
+        potentialFunc (func):
+        dq (float): Step to evaluate derivatives
+    """
+
+    force = np.zeros_like(q)
+
+    for i in range(q.shape[1]): # for each particle
+        force[:,i] = -approx_fprime(q[:, i], potentialFunc)
+
+    return force
+
+def noPotential(q):
+    return 0
