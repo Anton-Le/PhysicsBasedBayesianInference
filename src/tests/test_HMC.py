c27d801e61a215b6da09011ca2afd540f5e51f6f
ThomasWarford <64084106+ThomasWarford@users.noreply.github.com>
2022-08-09 22:54:19 +0100

    Update test_HMC.py
    Feature jax contour (#82)
    
    Added contour plot to test_HMC.py to replace numpy generated points.

--- src/tests/test_HMC.py
+++ src/tests/test_HMC.py
@@ -4,7 +4,7 @@
 """
 Created on Sun Jul 24 12:44:20 2022
 
-@author: thomas
+@author: bruno, thomas
 """
 import sys
 
@@ -16,6 +16,7 @@ import jax.numpy as jnp
 from matplotlib import pyplot as plt
 from scipy.constants import Boltzmann
 from jax.scipy.stats import multivariate_normal
+import matplotlib as mpl
 import jax
 from HMC import HMC
 
@@ -24,9 +25,9 @@ jax.config.update("jax_enable_x64", True)
 
 
 def test1():
-    seed = 1000
+    seed = 6
     key = jax.random.PRNGKey(seed)
-    numParticles = 4
+    numParticles = 2
     numDimensions = 2
 
     # PDF Setup
@@ -35,7 +36,8 @@ def test1():
     densityFunc = lambda q: multivariate_normal.pdf(q, mean, cov=cov)
     potentialFunc = lambda q: -multivariate_normal.logpdf(q, mean, cov=cov)
 
-    numIterations = 100
+    numIterations = 30
+
     subkeys = jax.random.split(key, numParticles)
     simulTime = 1
     stepSize = 0.01        
@@ -47,7 +49,6 @@ def test1():
     numpySamples = np.random.multivariate_normal(
         mean, cov, size=numParticles * numIterations
     )
-
 
     # get samples from HMC
 
@@ -66,43 +67,45 @@ def test1():
 
     fig, ax = plt.subplots()
 
-    ax.plot(
-        hmcSamples[0, 0],
-        hmcSamples[0, 1],
-        label="HMC",
-        marker="*",
-        c="k",
-        lw=0.2,
-        ls="-",
-        markersize=4,
-    )
 
-    for particleNum in range(1, numParticles):
+    colors=['black', 'red', 'blue', 'green']
+    for particleNum in range(numParticles):
         ax.plot(
             hmcSamples[particleNum, 0],
             hmcSamples[particleNum, 1],
             marker="*",
-            c="k",
+            color=colors[particleNum],
+            label=particleNum+1,
             lw=0.2,
             ls="-",
             markersize=4,
         )
 
-    ax.plot(
-        numpySamples[:, 0],
-        numpySamples[:, 1],
-        label="Numpy",
-        marker=".",
-        c="r",
-        lw=0.2,
-        ls="",
-        markersize=4,
+
+    # contour plot
+    x = np.linspace(mean[0]-7, mean[0]+7)
+    y = np.linspace(mean[1]-7, mean[1]+7)
+    x_mesh, y_mesh = np.meshgrid(x, y)
+    q = np.dstack((x_mesh, y_mesh))
+    z = densityFunc(q)
+
+
+    cmap = plt.get_cmap("Pastel1").copy()
+
+    contour = ax.contour(
+        x_mesh,
+        y_mesh,
+        z,
+        cmap=cmap,
+        zorder=0,
     )
+
+
 
     plt.title(r"Guassian with mean (5, 5)")
     plt.xlabel(r"$x_{1}$")
     plt.ylabel(r"$x_{2}$")
-    plt.legend(loc="upper right")
+    plt.legend(title='HMC Branch', loc="upper right")
     plt.show()
 
 
