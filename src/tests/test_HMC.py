a9b0de166e72854b438c3f475830d8b5eb0c26eb
Thomas Warford <thomasdwarford@hotmail.co.uk>
2022-08-21 13:36:25 +0100

    Changes to Ensemble class so setting of momenta ect. is done by ensemble.

--- src/tests/test_HMC.py
+++ src/tests/test_HMC.py
@@ -16,6 +16,7 @@ import jax.numpy as jnp
 from matplotlib import pyplot as plt
 from scipy.constants import Boltzmann
 from jax.scipy.stats import multivariate_normal
+from ensemble import Ensemble
 import matplotlib as mpl
 import jax
 from HMC import HMC
@@ -24,57 +25,63 @@ from HMC import HMC
 jax.config.update("jax_enable_x64", True) 
 
 
-def test1():
-    seed = 6
-    key = jax.random.PRNGKey(seed)
-    numParticles = 2
+def test():
+    # ensemble setup
+
+    numParticles = 3
     numDimensions = 2
+    temperature = 1 / Boltzmann
+    seed = 10
+    key = jax.random.PRNGKey(seed)  
+
+    ensemble = Ensemble(numParticles, numDimensions, temperature, key)
+    
 
     # PDF Setup
     mean = jnp.ones(numDimensions) * 5
     cov = jnp.array([[4, -3], [-3, 4]])  # random covariance matrix
     densityFunc = lambda q: multivariate_normal.pdf(q, mean, cov=cov)
-    potentialFunc = lambda q: -multivariate_normal.logpdf(q, mean, cov=cov)
+    potentialFunc = lambda q: -multivariate_normal.logpdf(q, mean, cov=cov)  
 
-    numIterations = 30
-
-    subkeys = jax.random.split(key, numParticles)
+    
+    # HMC setup
     simulTime = 1
-    stepSize = 0.01        
-    temperature = 1 / Boltzmann 
-    qStd = 3
-    mass = jnp.ones(numParticles)
+    stepSize = 0.01   
 
-    # get samples from numpy 
-    numpySamples = np.random.multivariate_normal(
-        mean, cov, size=numParticles * numIterations
-    )
-
-    # get samples from HMC
 
     hmcObject = HMC(
-        numDimensions,
         simulTime,
         stepSize,        
-        temperature, 
-        qStd,
         densityFunc,
         potential=potentialFunc,
         method='Stormer-Verlet'
     )
 
-    hmcSamples, _ = hmcObject.getSamples(numIterations, mass, subkeys)
+    # set positions
+
+    qStd = 3
+    ensemble.setPosition(3)
+
+    numIterations = 100
+
+    hmcSamples = jnp.zeros((numIterations, numParticles, numDimensions,))
+
+    for i in range(numIterations):
+        ensemble.setMomentum() # numParticles!
+
+        ensemble = hmcObject.propagate_ensemble(ensemble)
+
+        hmcSamples = hmcSamples.at[i].set(ensemble.q)
+
 
     fig, ax = plt.subplots()
 
 
-    colors=['black', 'red', 'blue', 'green']
     for particleNum in range(numParticles):
         ax.plot(
-            hmcSamples[particleNum, 0],
-            hmcSamples[particleNum, 1],
+            hmcSamples[:, particleNum, 0],
+            hmcSamples[:, particleNum, 1],
             marker="*",
-            color=colors[particleNum],
             label=particleNum+1,
             lw=0.2,
             ls="-",
@@ -82,13 +89,14 @@ def test1():
         )
 
 
+
     # contour plot
+
     x = np.linspace(mean[0]-7, mean[0]+7)
     y = np.linspace(mean[1]-7, mean[1]+7)
     x_mesh, y_mesh = np.meshgrid(x, y)
     q = np.dstack((x_mesh, y_mesh))
     z = densityFunc(q)
-
 
     cmap = plt.get_cmap("Pastel1")
 
@@ -101,13 +109,14 @@ def test1():
     )
 
 
+    ax.set_title(r"Guassian with mean (5, 5)")
+    ax.set_xlabel(r"$x_{1}$")
+    ax.set_ylabel(r"$x_{2}$")
+    ax.legend(title='HMC Branch', loc="upper right")
 
-    plt.title(r"Guassian with mean (5, 5)")
-    plt.xlabel(r"$x_{1}$")
-    plt.ylabel(r"$x_{2}$")
-    plt.legend(title='HMC Branch', loc="upper right")
     plt.show()
 
 
+
 if __name__ == "__main__":
-    hmcSamples = test1()  # change to test1 if desired
+    test()
