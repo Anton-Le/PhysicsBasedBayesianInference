cccf8be41f4e31ed45d71ff39ad32efc4a9211d3
Anton Lebedev <le_t@gmx.net>
2022-12-30 19:03:56 +0100

    Revert "Feature mpi (#125)"
    This reverts commit 1c9eb2fc5b9d80b0f1f63d0446f783457832469a.

--- src/main.py
+++ src/main.py
@@ -31,35 +31,7 @@ from stepSizeSelection import dtProposal
 # distribution of positions.
 from jax.scipy.stats import multivariate_normal
 
-import mpi4jax
-from mpi4py import MPI, rc
-rc.threaded = True
-rc.thread_level = "funneled"
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank ()
-size = comm.Get_size ()
-
 os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=1"
-
-def getWeightedMeanStd(estimate, Z):
-    '''
-    Get weighted mean from multiple ensembles and error.
-    '''
-    estimateTimesZ, _ = mpi4jax.gather(estimate * Z, root=0)
-    estimate, _ = mpi4jax.gather(estimate, root=0)
-    Z, _ = mpi4jax.gather(Z, root=0)
-    
-    if rank == 0:
-        #totalZ = jnp.sum(Z)
-        #weightedMean = jnp.sum(estimateTimesZ, axis=0) / totalZ
-        weightedMean = jnp.average(estimate, axis=0, weights=Z)
-        std = jnp.sqrt(
-            jnp.average((estimate-weightedMean)**2, axis=0, weights=Z)
-            )
-
-        return weightedMean, std
-    return (None, None)
-
 
 if __name__ == "__main__":
     # select the platform
@@ -68,29 +40,24 @@ if __name__ == "__main__":
     numpyro.set_platform(platform)
     # Print run-time configuration infromation
 
-    if rank == 0:
+    print(f"jax version: {jax.__version__}")
+    print(f"numpyro version: {numpyro.__version__}")
+    print(f"jax target backend: {jax.config.FLAGS.jax_backend_target}")
+    print(f"jax target device: {jax.lib.xla_bridge.get_backend().platform}")
+    devices = jax.devices(platform)
+    print("Available devices:")
+    print(devices)
 
-        print(f"jax version: {jax.__version__}")
-        print(f"numpyro version: {numpyro.__version__}")
-        print(f"jax target backend: {jax.config.FLAGS.jax_backend_target}")
-        print(f"jax target device: {jax.lib.xla_bridge.get_backend().platform}")
-        devices = jax.devices(platform)
-        print("Available devices:")
-        print(devices)
+    # load model data and set up the statistical model
+    # Load the observed outcomes and the reference biases
+    data = json.load(open("CoinToss.data.json"))
+    modelDataDictionary = {
+        "c1": np.array(data["c1"]),
+        "c2": np.array(data["c2"]),
+    }
+    p1_reference = float(data["p1"])
+    p2_reference = float(data["p2"])
 
-        # load model data and set up the statistical model
-        # Load the observed outcomes and the reference biases
-        data = json.load(open("CoinToss.data.json"))
-        modelDataDictionary = {
-            "c1": np.array(data["c1"]),
-            "c2": np.array(data["c2"]),
-        }
-        p1_reference = float(data["p1"])
-        p2_reference = float(data["p2"])
-    else:
-        modelDataDictionary = None
-
-    modelDataDictionary = comm.bcast(modelDataDictionary, root=0)
     model = coin_toss
     # CAVEAT: model arguments are an empty tuple here, subject to change!
     statModel = statisticalModel(model, (), modelDataDictionary)
@@ -113,7 +80,7 @@ if __name__ == "__main__":
     #finalTime = 0.1
     finalTime = inputArguments.t_final
     random_seed = 1234
-    rng_key = jax.random.PRNGKey(random_seed+rank)
+    rng_key = jax.random.PRNGKey(random_seed)
     seed(model, rng_key)
 
     # Set up the initial distribution of particles
@@ -135,20 +102,17 @@ if __name__ == "__main__":
     # compute initial mean values
     initialEstimate, initialZ = ensemble.getWeightedMean()
 
-    initialMean, initialStd = getWeightedMeanStd(initialEstimate, initialZ)
-    dtSizes = dtProposal(ensemble, statModel.potential )
+    dtSizes = dtProposal(ensemble, statModel.potential, stepSize )
     avgDt = jnp.average(dtSizes)
+    
+    print(f"Mean parameters after initialisation \n", initialEstimate)
 
-    if rank == 0:
-        print(f"Mean parameters after initialisation \n", f'{initialMean} +- {initialStd}')
-        print("Mean dt: ", f'{avgDt}')
-
-        print(
-            f"Mean parameters after initialisation, transformed: \n",
-            statModel.converter.toArray(
-                statModel.constraint_fn(statModel.converter.toDict(initialMean))
-            ),
-        )
+    print(
+        f"Mean parameters after initialisation, transformed: \n",
+        statModel.converter.toArray(
+            statModel.constraint_fn(statModel.converter.toDict(initialEstimate))
+        ),
+    )
 
     # HMC algorithm
     hmcObject = HMC(
@@ -163,24 +127,24 @@ if __name__ == "__main__":
 
     meanParameter = ensemble.getArithmeticMean()
     print("Arithmetic mean parameter: ", meanParameter)
-    finalMean, finalStd = getWeightedMeanStd(meanParameter, 1.0)
-    if rank == 0:
-        print(f"Mean parameters after HMC: \n", f'{finalMean} +- {finalStd}')
-        resultVector = statModel.converter.toArray(
-                statModel.constraint_fn(statModel.converter.toDict(finalMean))
+    resultVector = statModel.converter.toArray(
+                statModel.constraint_fn(statModel.converter.toDict(meanParameter))
             )
-        print(
+    print(
             f"Mean parameters after HMC, transformed: \n",
             resultVector,
-        )
-        p1 = resultVector[0]
-        p2 = resultVector[1]
-        # Since this is Markov-Chain monte Carlo with MH proposal
-        # We may use simple averaging to obtain the parameters
-        print("Bias of coin 1: ", p1)
-        print("Absolute error: ", abs(p1 - p1_reference))
-        print("Relative error: ", abs(p1 - p1_reference) / p1_reference)
+    )
+    p1 = resultVector[0]
+    p2 = resultVector[1]
+    # Since this is Markov-Chain monte Carlo with MH proposal
+    # We may use simple averaging to obtain the parameters
+    print("Bias of coin 1: ", p1)
+    print("Absolute error: ", abs(p1 - p1_reference))
+    print("Relative error: ", abs(p1 - p1_reference) / p1_reference)
 
-        print("Bias of coin 2: ", p2)
-        print("Absolute error: ", abs(p2 - p2_reference))
-        print("Relative error: ", abs(p2 - p2_reference) / p2_reference)
+    print("Bias of coin 2: ", p2)
+    print("Absolute error: ", abs(p2 - p2_reference))
+    print("Relative error: ", abs(p2 - p2_reference) / p2_reference)
+    print("Obtained samples: \n", ensemble.q)
+    meanParameter, Z = ensemble.getWeightedMean()
+    print(f"Mean parameters after HMC: \n", meanParameter)
